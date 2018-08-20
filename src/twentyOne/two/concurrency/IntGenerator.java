c740df60377a60d82f49072cde0adb26e93f59c7
panyuhang <a415706772@qq.com>
2018-08-20 22:48:43 +0800

    Revert "Revert "Revert "Revert "Initial commit""""
    This reverts commit 61a916918fdf9e68153b908aad50ec3c30b6f479.

---
+++ src/twentyOne/two/concurrency/IntGenerator.java
@@ -0,0 +1,12 @@
+package twentyOne.two.concurrency;
+
+public abstract class IntGenerator {
+	private volatile boolean canceled = false;
+	public abstract int next();
+	public void cancel() {
+		canceled = true;
+	}
+	public boolean isCanceled() {
+		return canceled;
+	}
+}
